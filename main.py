a582d9b6ff0f12be7d4061ca96c43fb401413f5e
kuro611 <kuropc1324@gmail.com>
2025-05-17 23:43:53 +0900

    お試し

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 from keep_alive import keep_alive
-from bot import run_bot  # bot.py で run_bot() を定義している前提
-
-if __name__ == "__main__":
-    keep_alive()
-    print("keep_alive呼び出し")
-    run_bot()
-    print("run_bot呼び出し")
+import bot_minimal  # ← 起動だけこのファイルに任せる
