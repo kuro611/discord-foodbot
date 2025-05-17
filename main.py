c3d292f7556a015e5ffda24f2f4d6e6e55786dcb
kuro611 <kuropc1324@gmail.com>
2025-05-18 00:02:01 +0900

    たのむって

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from keep_alive import keep_alive
 from bot_minimal import bot
+import os
 
 if __name__ == "__main__":
     keep_alive()
-    bot.run("あなたのトークン")
+    bot.run(os.environ.get("DISCORD_TOKEN"))
